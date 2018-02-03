5222ca8836920c48866db1b577b333b146e5b247
Kayla Webb <kaylarwebb@gmail.com>
2018-02-02 22:55:44 -0500

    Remove unsused components, render message when animals unavailable

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -5,16 +5,21 @@ import './Pet.css'
 
 export default function Pet(props){
   console.log('Pet Props', props)
+
+  let details = <ul className='detail-list'>
+  <li>Sex: {props.info.sex}</li>
+   <li>Age: {props.info.age}</li>
+   <li>Breed: {props.info.breed}</li>
+   <li>Story: {props.info.story}</li>
+ </ul>
+
+ let noneLeft = <p id='none-left'>{props.info.message}</p>
+//  style={{'display':(this.props.isModalOpen)?'block':'none'}}
 return (
     <section>
-      <Header animalName={props.info.name} imageURL={props.info.imageURL} imageAlt={props.info.imageDescription} />
+      {props.info.message ? null : <Header animalName={props.info.name} imageURL={props.info.imageURL} imageAlt={props.info.imageDescription} />}
       <main>
-        <ul className='detail-list'>
-         <li>Sex: {props.info.sex}</li>
-          <li>Age: {props.info.age}</li>
-          <li>Breed: {props.info.breed}</li>
-          <li>Story: {props.info.story}</li>
-        </ul>
+        {props.info.message ? noneLeft : details}
       </main>
       <Button buttonText={props.buttonText} onAdoptPet={props.onAdoptPet} />
     </section>
